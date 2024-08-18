79cbf21f5a4489283a93fc1fcf71e7e6e199a5b0
Alexandre <alexauragan@gmail.com>
2024-08-18 06:20:28 +0200

    fix ags

--- ags/.config/ags/modules/.widgethacks/popupwindow.js
+++ ags/.config/ags/modules/.widgethacks/popupwindow.js
@@ -18,17 +18,13 @@ export default ({
 
         child: Box({
             setup: (self) => {
-                self.keybind("Escape", () => closeEverything());
-                if (showClassName != "" && hideClassName !== "") {
-                    self.hook(App, (self, currentName, visible) => {
-                        if (currentName === name) {
-                            self.toggleClassName(hideClassName, !visible);
-                        }
-                    });
-
-                    if (showClassName !== "" && hideClassName !== "")
-                        self.className = `${showClassName} ${hideClassName}`;
-                }
+                self.hook(App, (self, currentName, visible) => {
+                    if (currentName === name) {
+                        self.toggleClassName(hideClassName, !visible);
+                    }
+                }).keybind("Escape", () => App.closeWindow(name))
+                if (showClassName !== "" && hideClassName !== "")
+                    self.className = `${showClassName} ${hideClassName}`;
             },
             child: child,
         }),
